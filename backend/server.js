bad88c5f0ec3224662f642b77d2edc9d6e219275
Anas <boungabanas123@gmail.com>
2026-08-17 03:00:07 +0100

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const { GoogleGenAI } = require("@google/genai");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
